be6d41350d93668fc01980c7aa65ef3bc215c7e0
Alasdair Bennett <alasdairjbbennett@gmail.com>
2021-07-06 17:42:39 -0400

    Add install directions

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
 # meta-pawn
-Chess metagame statistics explorer
+Chess metagame statistics explorer<br>
 <br>
 <br>
+Term project for COSC4427<br>
 <br>
-Term project of the newly reformed Team Toxic for COSC4427
+This project uses [this dataset](https://www.kaggle.com/datasnaek/chess) <br>
+
+
+<h2>Installation Directions:</h2>
+Requires python 3.9.5<br>
+
+From the project directory, run the following command to create a virtual environment:<br>
+`python3 -m venv venv`
 <br>
-<br>
-This project uses [this dataset](https://www.kaggle.com/datasnaek/chess)
+
+Run the activate script:<br>
+`venv/Scripts/activate.bat`<br>
+
+Install the required packages for the project:<br>
+`pip install -r requirements.txt`<br>
+
+Configure the environment variables:<br>
+`set FLASK_APP=app.py`<br>
+`set FLASK_ENV=development`<br>
+
+Run the app:<br>
+`flask run`<br>
+
+Navigate to `127.0.0.1:5000` 
--- app.py
+++ app.py
@@ -4,35 +4,37 @@ import pandas as pd
 app = create_app()
 
 
-###
-# # get_games takes the name of a file containing chess game data and returns a dataframe containing
-# #   only the data of ranked games
-# def get_games(filename):
-#     games = pd.read_csv('project/static/games.csv')
-#
-#     # prune the unranked games from the data set
-#     ranked_games = games[games['rated'] == True]
-#
-#     return ranked_games
-#
-#
-# # get_opening_frequency takes a dataframe containing chess match information and returns a dataframe
-# #   containing the most common chess openings in the match information dataframe
-# def get_opening_frequency(games):
-#     return games['opening_name'].value_counts()
-#
-#
-# if __name__ == "__main__":
-#     print("running...")
-#
-#     pd.set_option('display.max_columns', 20)
-#     pd.set_option('display.max_rows', 20)
-#     pd.set_option('display.width', 10000)
-#
-#     chess_games = get_games('project/static/games.csv')
-#     print(chess_games.iloc[:10])
-#     opening_freq = get_opening_frequency(chess_games)
-#     print(opening_freq[:10])
-#     rating = 1000
-#     black_lose_opening_freq = get_opening_frequency(chess_games[(chess_games.black_rating < rating) & (chess_games.winner == 'white')])
-#     print(black_lose_opening_freq[:10])
+# def frame_html
+
+##
+# get_games takes the name of a file containing chess game data and returns a dataframe containing
+#   only the data of ranked games
+def get_games(filename):
+    games = pd.read_csv('project/static/games.csv')
+
+    # prune the unranked games from the data set
+    ranked_games = games[games['rated'] == True]
+
+    return ranked_games
+
+
+# get_opening_frequency takes a dataframe containing chess match information and returns a dataframe
+#   containing the most common chess openings in the match information dataframe
+def get_opening_frequency(games):
+    return games['opening_name'].value_counts()
+
+
+if __name__ == "__main__":
+    print("running...")
+
+    pd.set_option('display.max_columns', 20)
+    pd.set_option('display.max_rows', 20)
+    pd.set_option('display.width', 10000)
+
+    chess_games = get_games('project/static/games.csv')
+    print(chess_games.iloc[:10])
+    opening_freq = get_opening_frequency(chess_games)
+    print(opening_freq[:10])
+    rating = 1000
+    black_lose_opening_freq = get_opening_frequency(chess_games[(chess_games.black_rating < rating) & (chess_games.winner == 'white')])
+    print(black_lose_opening_freq[:10])
