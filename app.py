4ed58d2dc0c58f4d8a3eee751ce78a31fbf39a09
Michael Buffone <44306347+BuffoneM@users.noreply.github.com>
2021-07-26 22:32:02 -0400

    Moved variables outside of main to be used into front-end

--- app.py
+++ app.py
@@ -11,6 +11,16 @@ app = create_app()
 chess_games = uf.get_games('project/static/games.csv')
 win_table_1 = uf.get_win_rate_table(uf.get_rel_game_set(chess_games, 1000))
 
+beginner_white_games = uf.get_beginner_white_games(chess_games)
+beginner_black_games = uf.get_beginner_black_games(chess_games)
+
+intermediate_white_games = uf.get_intermediate_white_games(chess_games)
+intermediate_black_games = uf.get_intermediate_black_games(chess_games)
+
+advanced_white_games = uf.get_advanced_white_games(chess_games)
+advanced_black_games = uf.get_advanced_black_games(chess_games)
+
+
 
 # TODO (re)move this code block - I'm not sure what's important here
 if __name__ == "__main__":
@@ -22,29 +32,10 @@ if __name__ == "__main__":
     print(uf.get_win_rate_table(chess_games))
     
     print("Beginner white games:")
-    beginner_white_games = uf.get_beginner_white_games(chess_games)
-    beginner_avg_delta = uf.get_avg_delta(beginner_white_games)
+    beginner_white_avg_delta = uf.get_avg_delta(beginner_white_games)
     print(beginner_white_games.sample(10))
-    print(beginner_avg_delta)
-    
-    print("Beginner black games:")
-    beginner_black_games = uf.get_beginner_black_games(chess_games)
-    #print(beginner_black_games.sample(10))
-    
-    print("Intermediate white games:")
-    intermediate_white_games = uf.get_intermediate_white_games(chess_games)
-    #print(intermediate_white_games.sample(10))
-    
-    print("Intermediate black games:")
-    intermediate_black_games = uf.get_intermediate_black_games(chess_games)
-    #print(intermediate_black_games.sample(10))
-    
-    print("Advanced white games:")
-    advanced_white_games = uf.get_advanced_white_games(chess_games)
-    #print(advanced_white_games.sample(10))
-    
-    print("Advanced black games:")
-    advanced_black_games = uf.get_advanced_black_games(chess_games)
-    #print(advanced_black_games.sample(10))
-     
+    print(beginner_white_avg_delta.sample(10))
     uf.get_winning_countplot(beginner_white_games)
+    
+    print(uf.get_opening_outliers(chess_games))
+    
