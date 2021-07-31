951bc70dfc18a5a2b99b4d10fdef4b5affa7f219
Duc Tran <dtran@algomau.ca>
2021-07-31 19:01:48 -0400

    rename get_recommended_w to get_ad_recommend_w

--- utility_functions.py
+++ utility_functions.py
@@ -117,7 +117,7 @@ def get_game_set_by_rating(game_set, rating):
                     (game_set['black_rating'] >= rating)].copy()
 
 
-def get_recommended_w(skill_val, novel_val):
+def get_ad_recommend_w(skill_val, novel_val):
     # cluster attributes
     # skill_val 1: beginner, 2: intermediate, 3: advanced
     cluster_attr = {
@@ -144,7 +144,7 @@ def get_recommended_w(skill_val, novel_val):
     return top_openings.sample(n=5, weights=p).drop(['opening_eco', 'cluster'], axis=1).reset_index(drop=True)
 
 
-def get_recommended_b(skill_val, novel_val):
+def get_ad_recommend_b(skill_val, novel_val):
     # cluster attributes
     # skill_val 1: beginner, 2: intermediate, 3: advanced
     cluster_attr = {
